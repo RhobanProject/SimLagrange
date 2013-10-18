93589f6ac29217acfda9dde959a3c13a8e0b1172
Leph <leph.quentinrouxel@gmail.com>
2013-10-18 16:31:40 +0200

    Adding Minus and Sub operation

---
+++ Symbolic/src/terms/Minus.hpp
@@ -0,0 +1,51 @@
+#ifndef LEPH_SYMBOLIC_MINUS_HPP
+#define LEPH_SYMBOLIC_MINUS_HPP
+
+#include "Symbolic/src/UnaryFunction.hpp"
+
+namespace Leph {
+namespace Symbolic {
+
+/**
+ * Minus
+ */
+template <class T>
+class Minus : public UnaryFunction<T,T>
+{
+    public:
+
+        static inline typename Term<T>::TermPtr create(
+            typename Term<T>::TermPtr term)
+        {
+            return typename Term<T>::TermPtr(
+                new Minus<T>(term));
+        }
+    protected:
+        
+        Minus(const typename Term<T>::TermPtr& term) :
+            UnaryFunction<T,T>(term)
+        {
+        }
+        
+        virtual inline std::string functionString() const
+        {
+            return "-";
+        }
+
+        virtual inline typename Term<T>::TermPtr functionderivative
+            (const typename Term<T>::TermPtr arg) const
+        {
+            return Minus<T>::create(arg);
+        }
+
+        virtual inline T functionEvaluation(const T& argVal) const
+        {
+            return -argVal;
+        }
+};
+
+}
+}
+
+#endif
+
---
+++ Symbolic/src/terms/Sub.hpp
@@ -0,0 +1,73 @@
+#ifndef LEPH_SYMBOLIC_SUB_HPP
+#define LEPH_SYMBOLIC_SUB_HPP
+
+#include "Symbolic/src/BinaryFunction.hpp"
+#include "Symbolic/src/terms/Minus.hpp"
+
+namespace Leph {
+namespace Symbolic {
+
+/**
+ * Sub
+ */
+template <class T>
+class Sub : public BinaryFunction<T,T,T>
+{
+    public:
+
+        static inline typename Term<T>::TermPtr create(
+            typename Term<T>::TermPtr termLeft, 
+            typename Term<T>::TermPtr termRight)
+        {
+            if (termLeft->toString() == BaseSymbol::zero()) {
+                return Minus<T>::create(termRight);
+            } else if (termRight->toString() == BaseSymbol::zero()) {
+                return termLeft;
+            } else {
+                return typename Term<T>::TermPtr(
+                    new Sub<T>(termLeft, termRight));
+            }
+        }
+
+    protected:
+
+        Sub(const typename Term<T>::TermPtr& termLeft, 
+            const typename Term<T>::TermPtr& termRight) :
+            BinaryFunction<T,T,T>(termLeft, termRight)
+        {
+        }
+
+        virtual inline std::string computeString()
+        {
+            return "(" + BinaryFunction<T,T,T>::_argLeft->toString()
+                + ")-(" + BinaryFunction<T,T,T>::_argRight->toString() + ")";
+        }
+        
+        virtual inline typename Term<T>::TermPtr computeDerivative
+            (const BaseSymbol::BaseSymbolPtr& sym)
+        {
+            typename Term<T>::TermPtr argLeft = 
+                BinaryFunction<T,T,T>::_argLeft;
+            typename Term<T>::TermPtr argRight = 
+                BinaryFunction<T,T,T>::_argRight;
+            typename Term<T>::TermPtr termLeft = 
+                argLeft->derivate(sym);
+            typename Term<T>::TermPtr termRight = 
+                argRight->derivate(sym);
+
+            return Sub<T>::create(termLeft, termRight);
+        }
+        
+        virtual T computeEvaluation(const Bounder& bounder)
+        {
+            T left = BinaryFunction<T,T,T>::_argLeft->evaluate(bounder);
+            T right = BinaryFunction<T,T,T>::_argRight->evaluate(bounder);
+            return left - right;
+        }
+};
+
+}
+}
+
+#endif
+
