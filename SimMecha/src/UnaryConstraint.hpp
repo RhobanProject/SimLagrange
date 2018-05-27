549e5679b08edc6d691aca890ba827d6d8889beb
Leph <leph.quentinrouxel@gmail.com>
2018-05-27 23:14:07 +0100

    Implementation of collision response by linear equation solving and test

--- SimMecha/src/UnaryConstraint.hpp
+++ SimMecha/src/UnaryConstraint.hpp
@@ -38,154 +38,156 @@ class UnaryConstraint : public Constraint
         {
         }
 
-
-        inline bool getConstraint(Vector2D& point, Vector2D& dir, Vector2D& posInBody)
+        /**
+         * Detect if the constraint is violated.
+         * In case of collision, a bijection search is used to 
+         * back in past and to refine the collision time
+         * and system state at this time.
+         * Internal System state is updated.
+         * WARNING: therefore, the simulation time step is 
+         * NOT CONSTANT when a collision occurs.
+         *
+         * @param point Assigned collision point in world frame in case of collision.
+         * @param dir Assigned contact normal in body frame in case of collision.
+         * @param posInBody Assigned collision point in body frame in case of collision.
+         * @param dt Normal simulation time step (for bijection search).
+         * @return true if a constraint violation is detected.
+         */
+        inline bool getConstraint(Vector2D& point, Vector2D& dir, Vector2D& posInBody, double dt)
         {
+            //Check if the constraint is currently violated
             if (!computeCheckConstraint(point, dir, posInBody)) {
                 return false;
             }
 
-            //TODO Bijection
-            scalar timeMin = 0.0;
-            scalar timeMax = 0.01;
-            scalar currentTime = 0.0;
-            for (int i=0;i<100;i++) { //FIXME?
-                scalar t = (timeMax-timeMin)/2.0;
-                _system->runSimulationStep(currentTime-t);
-                currentTime = t;
-                if (computeCheckConstraint(point, dir, posInBody)) {
-                    timeMin = t;
-                } else {
-                    timeMax = t;
-                }
-                // std::cout<<"DEBUG Col: "<<t<<" "<<timeMin<<" "<<timeMax<<std::endl;
+            //Retrieve state before last simulation step
+            std::vector<scalar> vectPos = _system->getLastPositions();
+            std::vector<scalar> vectVel = _system->getLastVelocities();
+
+            //Check that the constraint was not violated at last iteration
+            _system->setState(vectPos, vectVel);
+            _system->runSimulationStep(0.0);
+            bool isCollision1 = computeCheckConstraint(point, dir, posInBody);
+            _system->setState(vectPos, vectVel);
+            _system->runSimulationStep(dt);
+            bool isCollision2 = computeCheckConstraint(point, dir, posInBody);
+            if (isCollision1 || !isCollision2) {
+                std::cerr << "Constraint bijection sanity check failed" << std::endl;
+                return true;
             }
-            // std::cout << "TTT> " << currentTime << " " << timeMax << std::endl;
-            _system->runSimulationStep(currentTime-timeMax);
+
+            //Bijection to refine collision time
+            scalar stepMin = 0.0;
+            scalar stepMax = dt; 
+            scalar step;
+            for (int i=0;i<50;i++) { //50 is enough to reach double precision limit
+                step = (stepMax+stepMin)/2.0;
+                _system->setState(vectPos, vectVel);
+                _system->runSimulationStep(step);
+                if (computeCheckConstraint(point, dir, posInBody)) {
+                    stepMax = step;
+                } else {
+                    stepMin = step;
+                }
+            }
+
+            //Go back to collision time
+            _system->setState(vectPos, vectVel);
+            _system->runSimulationStep(step);
             computeCheckConstraint(point, dir, posInBody);
             return true;
         }
 
         /**
-         * TODO TODO TODO
+         * Check if the constraint is violated.
+         * In case of collision, accurate time of collision
+         * is refine and impulsion response is computed.
          */
-        inline virtual void handle()
+        inline virtual void handle(double dt)
         {
+            //Check for constraint violation
             Vector2D point;
             Vector2D dir;
             Vector2D posInBody;
-
-                /*
-            if (!computeCheckConstraint(point, dir, posInBody)) {
+            if (!getConstraint(point, dir, posInBody, dt)) {
                 return;
             }
 
-            //TODO Bijection
-            scalar timeMin = 0.0;
-            scalar timeMax = 0.01;
-            scalar currentTime = 0.0;
-            for (int i=0;i<10;i++) { //FIXME?
-                scalar t = (timeMax-timeMin)/2.0;
-                _system->runSimulationStep(currentTime-t);
-                currentTime = t;
-                if (computeCheckConstraint(point, dir, posInBody)) {
-                    timeMin = t;
-                } else {
-                    timeMax = t;
-                }
-            }
-            std::cout << "TTT> " << currentTime << " " << timeMax << std::endl;
-            _system->runSimulationStep(currentTime-timeMax);
-            computeCheckConstraint(point, dir, posInBody);
-                */
+            //Compute expected contact velocity in 
+            //world after collision impulsion
+            Vector2D targetVel = computeTargetContactVelocity(posInBody, dir);
 
-
-
-                //TODO: Resting contact
-                //FIXME: moving bodies?
-                /*
-            Vector2D centerPos = _system->evalPosition(*_body);
-            Vector2D centerVel = _system->evalPositionVel(*_body);
-            scalar centerAngleVel = _system->evalAngleVel(*_body);
-            Vector2D vel = centerVel
-                + centerAngleVel*Vector2D::normal(point - centerPos);
-
-            if(vel.norm()<_CONTACT_THRES)
-            {
-
-            }
-                */
-            //
-
-
-            if (!getConstraint(point, dir, posInBody)) {
-                return;
-            }
-
-
-            std::cout << "pouet" << std::endl;
-            std::cout << point << std::endl;
-
-            Vector2D impulse = computeImpulsion(
-                point, dir);
-            std::cout << "Impulse: "<<impulse<<"Direction: "<<dir << std::endl;
-            propagateImpulse(impulse, point, posInBody);
-        }
-
-        /**
-         * TODO
-         */
-        inline void propagateImpulse(const Vector2D& impulse,
-            const Vector2D& point, const Vector2D& posInBody)
-        {
-            //Get global coordinate Symbolic velocity of
-            //collision point
-            TermVectorPtr currentVelSym =
-                _body->buildSymPositionVel(posInBody);
-            //Bind position Symbol
-            currentVelSym =
-                _system->bindStatePosition(currentVelSym);
-            //Get binded system lagrangian
-            TermPtr lagrangian =
-                _system->getBindedLagrangian();
-            //Get velocity degrees of freedom Symbols
+            //Compute system Jacobian matrix at 
+            //contact point and system inertia mass matrix
+            EigenMatrix matJac = _system->evalJacobian(*_body, posInBody);
+            EigenMatrix matH = _system->evalInertiaMatrix();
+            unsigned int ndof = matH.rows();
+            
+            //Retrieve in Eigen vector current system 
+            //DoFs velocity vector
+            EigenVector velOld(ndof);
             const DofContainer& dofs =
                 _system->getVelocityDofs();
-
-            //Build impulse constraint
-            TermVectorPtr constraint = Symbolic::Sub<Vector2D>::create(
-                currentVelSym,
-                ConstantVector::create(impulse));
-            TermPtr constraintX = Symbolic::X<scalar,Vector2D>::
-                create(constraint);
-            TermPtr constraintY = Symbolic::Y<scalar,Vector2D>::
-                create(constraint);
-
-            std::cout << currentVelSym->toString() << std::endl;
-            //std::cout << lagrangian->toString() << std::endl;
             for (size_t i=0;i<dofs.size();i++) {
-                std::cout << dofs.getKey(i) << std::endl;
+                velOld(i) = _system->stateVelocity(dofs.getKey(i));
             }
 
-            //Initialize Optimization target function and
-            //constraints
-            SymOptim::PenaltyMethod<scalar> penaltyOptim(
-                lagrangian, dofs);
-            penaltyOptim.addEqualityConstraint(constraintX);
-            penaltyOptim.addEqualityConstraint(constraintY);
-            //Initialize point
-            for (size_t i=0;i<dofs.size();i++) {
-                penaltyOptim.state(dofs.getKey(i)) = 0.0;
+            /**
+             * When a collision occurs, the response is computed 
+             * by applying an impulsion to the system (immediate
+             * modification of degrees of freedom velocity).
+             * Given the old (before collision) DoF velocities, 
+             * the old Cartesian velocity at contact point and 
+             * the new expected velocity at contact point, the
+             * new DoF velocities under momentum conservation.
+             * J: Jacobian at contact point.
+             * H: Inertia matrix.
+             * dq-: Old DoF velocities.
+             * dq+: New DoF velocities.
+             * v-:  Old Cartesian contact point velocity.
+             * v+:  New Cartesian contact point velocity.
+             * i:   Cartesian impulsion at contact point.
+             * J.dq- = v-
+             * J.dq+ = v+
+             * H.dq+ = H.dq- + J'.i
+             *
+             * The following linear problem is solved:
+             * |H J'|.|dq+| = |H.dq-|
+             * |J 0 | |-i |   |v+|
+             */
+
+            EigenMatrix problemMat;
+            EigenVector problemVec;
+            if (!Constraint::isFriction()) {
+                //When tangent friction is disabled,
+                //only normal final velocity is enforced
+                problemMat = EigenMatrix::Zero(ndof+1, ndof+1);
+                problemMat.block(0, 0, ndof, ndof) = matH;
+                problemMat.block(ndof, 0, 1, ndof) = matJac.block(1, 0, 1, ndof);
+                problemMat.block(0, ndof, ndof, 1) = matJac.block(1, 0, 1, ndof).transpose();
+                problemVec = EigenVector::Zero(ndof+1);
+                problemVec.segment(0, ndof) = matH*velOld;
+                problemVec(ndof+0) = targetVel.y();
+            } else {
+                //When tangent friction is enabled,
+                //tangent final velocity is enforced to zero.
+                //Note that partial tangent friction is possible.
+                problemMat = EigenMatrix::Zero(ndof+2, ndof+2);
+                problemMat.block(0, 0, ndof, ndof) = matH;
+                problemMat.block(ndof, 0, 2, ndof) = matJac;
+                problemMat.block(0, ndof, ndof, 2) = matJac.transpose();
+                problemVec = EigenVector::Zero(ndof+2);
+                problemVec.segment(0, ndof) = matH*velOld;
+                problemVec(ndof+0) = 0.0;
+                problemVec(ndof+1) = targetVel.y();
             }
-            //Run optimization
-            std::cout << "Start Optim" << std::endl;
-            penaltyOptim.runOptimization(true);
-            //Get result TODO
+            
+            //Solve linear equation
+            EigenVector velNew = problemMat.partialPivLu().solve(problemVec);
+
+            //Assign back DoF velocities to system
             for (size_t i=0;i<dofs.size();i++) {
-                std::cout << dofs.getKey(i) << " " <<
-                    penaltyOptim.state(dofs.getKey(i)) << std::endl;
-                _system->stateVelocity(dofs.getKey(i)) +=
-                    penaltyOptim.state(dofs.getKey(i));
+                _system->stateVelocity(dofs.getKey(i)) = velNew(i);
             }
         }
 
@@ -221,43 +223,36 @@ class UnaryConstraint : public Constraint
             Vector2D& point, Vector2D& dir, Vector2D& posInBody) = 0;
 
         /**
-         * Compute and return the velocity impulsion
-         * given the contact point (in global frame)
-         * and collision normal direction
+         * Compute and return the expected velocity of given 
+         * contact point in body frame after the collision impulsion
          */
-        inline Vector2D computeImpulsion
-            (const Vector2D& point, const Vector2D& dir) const
+        inline Vector2D computeTargetContactVelocity
+            (const Vector2D& posInBody, const Vector2D& dir) const
         {
             if (dir.norm() < 0.999 || dir.norm() > 1.001) {
                 throw std::logic_error(
                     "UnaryConstraint not normalized dir");
             }
 
-            //Compute velocity of collisioning point
-            Vector2D centerPos = _system->evalPosition(*_body);
-            Vector2D centerVel = _system->evalPositionVel(*_body);
-            scalar centerAngleVel = _system->evalAngleVel(*_body);
-            Vector2D vel = centerVel
-                + centerAngleVel*Vector2D::normal(point - centerPos);
+            //Compute the current of velocity 
+            //of contacting point in word frame
+            Vector2D vel = _system->evalPointVelocity(*_body, posInBody);
 
-                //FIXME: between moving bodies?
-
-
-            //Build impulse
-            Vector2D impulse(0.0, 0.0);
+            //Build expected contact velocity
+            Vector2D targetVel(0.0, 0.0);
+            scalar e = Constraint::getRestitutionCoef();
 
             //Declare normal and tangent unit vector
             Vector2D n = dir;
             Vector2D t = Vector2D::normal(dir);
             //Tangent (friction) component
-            if (Constraint::isFriction()) {
-                impulse += -Vector2D::dot(vel, t)*t;
+            if (!Constraint::isFriction()) {
+                targetVel += Vector2D::dot(vel, t)*t;
             }
             //Normal (response) component
-            scalar e = Constraint::getRestitutionCoef();
-            impulse += -(e+1.0)*Vector2D::dot(vel, n)*n;
+            targetVel += -e*Vector2D::dot(vel, n)*n;
 
-            return impulse;
+            return targetVel;
         }
 };
 
